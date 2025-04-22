ddeeb63ba80b8fb9684cbee63124560928d68576
dhayanandhs2003 <dhaya.pit180@gmail.com>
2025-04-22 16:54:36 +0530

    added sample tables and rows into the table

--- DB_Queries.sql
+++ DB_Queries.sql
@@ -11,3 +11,18 @@ create table admin(
 insert into admin (username, password) values ('dhaya','dhaya1234'); 
 
 select * from admin;
+
+create table students(
+    student_id int auto_increment primary key,
+    name varchar(100) not null,
+    age int,
+    grade varchar(10),
+    email varchar(100),
+    phone varchar(15),
+    address text
+);
+
+insert into students(name, age, grade, email, phone, address) values
+('Aarav', 15, '10', 'aarav@gmail.com', '2956147874', 'Chennai'),
+('Diya Sharma', 14, '9', 'diya@example.com', '9123456780', 'Coimbatore');
+
